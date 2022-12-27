0a5d5cc791c6fcd0fab5bea59afb956a35dc60b9
miceremwirigi <miceremwirigi@gmail.com>
2022-12-28 00:48:05 +0300

    removed sensitive info

--- PLP/Python/django/Real estate website/src/real_estate/settings.py
+++ PLP/Python/django/Real estate website/src/real_estate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-sr_e@87m97a)7*g$(#u^$#!hps4_i-_7cj1smjsn!7$(r5=!o4"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG",cast=bool)
 
 ALLOWED_HOSTS = []
 
