e8084d58ebfec1efdefed8007f9f966928cd277f
miceremwirigi <miceremwirigi@gmail.com>
2023-01-05 21:11:44 +0300

    Add to and clear from cart

--- PLP/Web/Todo/practice.js
+++ PLP/Web/Todo/practice.js
@@ -12,10 +12,14 @@ todos.forEach(function(todo) {
     document.body.appendChild(element);
     });
 
+
+    //console logs a string in uppercase
 function toUpper(stringList) {
     stringList.forEach(function(string) { 
         console.log(string.toUpperCase());
     })}
+
+    //makes and prints a list of double strings in console
 function arrayDouble(stringList) {
     let newArray = [];
     stringList.forEach(function(string) {
@@ -24,6 +28,8 @@ function arrayDouble(stringList) {
     })
     console.log(newArray);
     }
+
+    //adds numbers contained in a list and prins sum in console
 function arraySum(numberList) {
     let total = 0;
     numberList.forEach(function(number) {
@@ -36,6 +42,8 @@ function arraySum(numberList) {
 ///arrayDouble(todos);
 ///arraySum([1,2,3,4,5])
 
+
+//changes the inertext of a button
 function printDone(){
     let button = document.getElementById('todo-button'); // Fun Toggle button
     if (button.innerText != 'Done'){
@@ -47,7 +55,6 @@ function printDone(){
 }
  
 // Simple up and down counter
-
 let counterElement = document.createElement('div');
 counterElement.id = 'counter';
 counterElement.innerHTML = '0';
@@ -77,12 +84,48 @@ document.getElementById('counter-button-down').onclick = function countDown(){
     document.getElementById('counter').innerHTML = count;
 }
 
-// Adding input text to page content 
 
-
+// Tkaes input text, creates div and adds div to page content 
 function updateText() {  
     let readInputElement = document.getElementById('text-input');     
     let addTextElement = document.createElement('div');
     addTextElement.innerHTML = readInputElement.value;
     document.body.appendChild(addTextElement);
     }
+
+// Cart to add and delete fruit selection as a div to content page
+
+let addCart = document.createElement('div');
+addCart.id = 'cart';
+document.body.appendChild(addCart);
+
+cartList = ["Apple","Egg","Tomatoes","Oranges","Citrus"]
+cartList.forEach(cartItem => {
+    addButton(cartItem);
+    addToCart(cartItem);
+}
+)
+addButton("clear");
+deleteCart();
+
+
+
+function addButton(text) {
+    let button = document.createElement('button');
+    button.id = text.toLowerCase();
+    button.innerText = text;
+    document.body.appendChild(button);    
+}
+function addToCart(text) {
+    let button = document.getElementById(text.toLowerCase());
+    let cartItem = document.createElement('div');
+    let cart = document.getElementById('cart');
+    cartItem.innerText = button.innerText
+    button.onclick = () => {cart.appendChild(cartItem)};
+}
+function deleteCart(){
+    let button = document.getElementById("clear");
+    let cart = document.getElementById('cart');
+    
+    button.onclick = () => {cart.innerHTML = ""};
+}
